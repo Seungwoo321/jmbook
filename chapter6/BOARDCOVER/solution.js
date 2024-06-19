45f4b56b18baa89c062ad1b791f787d500860091
unknown <seungwoo321@gmail.com>
2024-06-19 23:21:44 +0900

    study: solve BOARDCOVER

--- chapter6/BOARDCOVER/solution.js
+++ chapter6/BOARDCOVER/solution.js
@@ -14,7 +14,7 @@ function solution (h, w, arr) {
             visited[i][j] = +(arr[i][j] === '#');
         }
     }
-    console.log(h, w, arr, visited)
+    // console.log(h, w, '\n' + visited.map(r => r.join(' ')).join('\n'))
     // 주어진 칸을 덮을 수 있는 4가지 방법
     const coverType = [
         [[0, 0], [1, 0], [0, 1]],
@@ -24,25 +24,21 @@ function solution (h, w, arr) {
     ];
     // 겹치거나 검은 칸을 덮을 때 false를 반환한다.
     const set = (board, y, x, type, delta) => {
+        let ok = true;
         for (let i = 0; i < 3; i ++) {
             const [dy, dx] = coverType[type][i];
             const ny = dy + y;
             const nx = dx + x;
             if (ny < 0 || ny >= h || nx < 0 || nx >= w) {
-                return false
+                ok = false
+            } else if ((board[ny][nx] += delta) > 1)  {
+                ok = false;
             }
-            // console.log(board[ny][nx])
-            board[ny][nx] += delta
-            if (board[ny][nx] > 1)  {
-                return false;
-            }
-            return true
         }
+        return ok
     }
 
     const cover = (board) => {
-        // console.log(board.map(r => r.join('')).join('\n'))
-        // console.log()
         // 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
         let y = -1;
         let x = -1;
@@ -63,17 +59,15 @@ function solution (h, w, arr) {
             if (set(board, y, x, type, 1)) {
                 result += cover(board);
             }
-            set(board, y, x, type, -1)
             // 덮었던 블록을 치운다
+            set(board, y, x, type, -1)
         }
         return result;
     }
-    // console.log(visited.map(r => r.join('')).join('\n'))
-    // console.log()
     return cover(visited)
 }
 
-for (let i = 0; i < 1; i ++) {
+for (let i = 0; i < tests; i ++) {
     const [H, W] = input[i].split(' ');
     console.log(solution(+H, +W, input.splice(i + 1, +H)));
 }
