dd221a54f32d0be5344e3f3de0b0f1fdb4310372
Seungwoo321 <seungwoo321@gmail.com>
2024-07-04 22:35:22 +0900

    study: add wildcard

--- README.md
+++ README.md
@@ -29,4 +29,5 @@
 - [외발 뛰기 (문제 ID: JUMPGAME,  난이도: 하) - 재귀 호출 알고리즘](/chapter8/JUMPGAME/jump.js)
 - [외발 뛰기 (문제 ID: JUMPGAME,  난이도: 하) - 동적 계획법 알고리즘](/chapter8/JUMPGAME/jump2.js)
 - [와일드카드 (문제 ID: WILDCARD, 난이도: 중) - 완전 탐색 알고리즘](/chapter8/WILDCARD/match.js)
-- [와일드카드 (문제 ID: WILDCARD, 난이도: 중) - 동적 계획법 알고리즘](/chapter8/WILDCARD/matchMemoized.js)
+- [와일드카드 (문제 ID: WILDCARD, 난이도: 중) - 동적 계획법 알고리즘1 - O(n^3)](/chapter8/WILDCARD/matchMemoized.js)
+- [와일드카드 (문제 ID: WILDCARD, 난이도: 중) - 동적 계획법 알고리즘2 - 다른 분해 방법 O(n^2)](/chapter8/WILDCARD/matchMemoized.js)
---
+++ chapter8/WILDCARD/matchMemoized2.js
@@ -0,0 +1,40 @@
+const filePath = require('path').join(__dirname, 'input');
+let [testCase, ...input] = require('fs')
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution (word, str) {
+    const matchMemoized = (cache, W, S, w, s) => {
+        let result = cache[w][s];
+        if (result !== -1) {
+            return result;
+        }
+        if (s < S.length && w < W.length && (W[w] === '?' || W[w] === S[s])) {
+            return result = matchMemoized(cache, W, S, w + 1, s + 1);
+        }
+        if (w === W.length) {
+            return result = +(s === S.length);
+        }
+        if (W[w] === '*') {
+            if (matchMemoized(cache, W, S, w +1, s) || (s < S.length && matchMemoized(cache, W, S, w, s + 1))) {
+                return result = 1
+            }
+        }
+        return result = 0;
+        
+    }
+    return str.filter(S => {
+        const maxLen = Math.max(word.length, S.length) + 1;
+        const cache = Array.from({ length: maxLen }, () => new Array(maxLen).fill(-1));
+        return matchMemoized(cache, word, S, 0, 0);
+    }).join('\n')
+}
+
+while (testCase--) {                                                                                                                    
+    const pattern = input.shift();
+    const fileSize = +input.shift();
+    
+    console.log(solution(pattern, input.splice(0, fileSize)))
+}
