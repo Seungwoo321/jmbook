48886d85c0d5933ceec47703df2b593806e493e1
unknown <seungwoo321@gmail.com>
2024-07-13 22:15:53 +0900

    study: dp - TRIPATHCNT

--- chapter8/TRIPATHCNT/solution.js
+++ chapter8/TRIPATHCNT/solution.js
@@ -3,26 +3,27 @@ function solution(n, arr) {
     const triangle = arr.map(r => r.split(' ').map(Number));
     const cache = Array.from({ length: n }, () => new Array(n).fill(-1));
     const path = (y, x) => {
-        if (y === n - 1) return triangle[y][x];
-        if (cache[y][x] !== -1) return cache[y][x];
-        return cache[y][x] = Math.max(path(y + 1, x), path(y + 1, x + 1)) + triangle[y][x];
+      if (y === n - 1) return triangle[y][x];
+      if (cache[y][x] !== -1) return cache[y][x];
+      cache[y][x] = Math.max(path(y + 1, x), path(y + 1, x + 1)) + triangle[y][x];
+      return cache[y][x]
     }
     const countCache = Array.from({ length: n }, () => new Array(n).fill(-1));
     const count = (y, x) => {
-        if (y === n - 1) return 1;
-        let result = countCache[y][x];
-        if (result !== -1) return result;
-        result = 0;
-        // result = 0;
-        result += Math.max(path(y + 1, x + 1), path(y + 1, x))
-        // if (count(y + 1, x + 1) >= count(y + 1, x)) {
-        //     result += count(y + 1, x + 1)
-        // }
-        // if (count(y + 1, x + 1) <= count(y + 1, x)) {
-        //     result += count(y + 1, x)
-        // }
+      if (y === n - 1) return 1;
+      if (countCache[y][x] !== -1) {
         console.log(countCache)
-        return result
+        return countCache[y][x];
+      }
+      countCache[y][x] = 0;
+      // countCache[y][x] += Math.max(path(y + 1, x + 1), path(y + 1, x))
+      if (path(y + 1, x + 1) >= path(y + 1, x)) {
+          countCache[y][x] += count(y + 1, x + 1)
+      }
+      if (path(y + 1, x + 1) <= path(y + 1, x)) {
+          countCache[y][x] += count(y + 1, x)
+      }
+      return countCache[y][x]
     }
     return count(0, 0)
 }
